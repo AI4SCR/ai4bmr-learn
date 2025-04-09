ea8304d282baa30ee883013a2d73ea050f6d48dd
Adriano Martinelli <adrianom@student.ethz.ch>
2025-04-09 22:28:18 +0200

    cluster utilities; plotting utilities for patches; NestedCV dataset

---
+++ src/ai4bmr_learn/clustering/cluster.py
@@ -0,0 +1,117 @@
+# %%
+import igraph as ig
+import leidenalg as la
+import numpy as np
+import pandas as pd
+import scipy.sparse
+import umap
+from numba import cuda
+from loguru import logger
+
+
+def cluster(
+    data: pd.DataFrame,
+    metadata: pd.DataFrame,
+    resolution: float = 1,
+    graph_method: str = "umap",
+    n_neighbors: int = 15,
+    cluster_method: str = "leiden",
+):
+    assert data.index.equals(metadata.index)
+    logger.info(f"cluster data with {len(data)} observations")
+    logger.info(f"compute graph with method `{graph_method}`")
+
+    # %%
+    if graph_method == "umap":
+        min_dist = 0.1
+        metric = "euclidean"
+        n_components = 2
+
+        mapper = umap.UMAP(
+            n_neighbors=n_neighbors,
+            min_dist=min_dist,
+            metric=metric,
+            n_components=n_components,
+            n_jobs=-1,
+        )
+        mapper.fit(data)
+        csr = mapper.graph_
+    elif graph_method == "knn":
+        from sklearn.neighbors import kneighbors_graph
+        metric = "euclidean"
+        csr = kneighbors_graph(data, n_neighbors=n_neighbors, mode='distance', metric=metric, include_self=False)
+        # assert False, 'we need to convert distance to weights'
+    elif graph_method == "scanpy":
+        from anndata import AnnData
+        if cuda.is_available():
+            logger.info("GPU available")
+            import rapids_singlecell as sc
+        else:
+            import scanpy as sc
+
+        # %% create AnnData
+        ad = AnnData(X=data.values, obs=metadata)
+
+        if cuda.is_available():
+            sc.get.anndata_to_GPU(ad)
+        sc.pp.neighbors(ad, n_neighbors=n_neighbors, use_rep="X")
+        csr = ad.obsp["connectivities"]
+
+    G = csr_to_ig(csr)
+
+    if cluster_method == "leiden":
+        membership = get_leiden_membership(G, resolution=resolution)
+    elif cluster_method == "igraph":
+        membership = get_igraph_membership(G, resolution=resolution)
+    elif cluster_method == "kmeans":
+        n_clusters = 20
+        membership = get_kmeans_membership(data, n_clusters=n_clusters)
+    elif cluster_method == "scanpy":
+        assert graph_method == "scanpy"
+        sc.tl.leiden(ad, resolution=resolution)
+        # sc.tl.leiden(ad, resolution=resolution, flavor="igraph", n_iterations=2)
+        membership = ad.obs["leiden"].astype(str).values
+
+    membership = pd.Categorical([str(i) for i in membership])
+
+    num_cluster = np.unique(membership).size
+    metadata = metadata.assign(membership=membership)
+    result = dict(num_obs=len(data), num_cluster=num_cluster)
+
+    logger.info("completed")
+    return metadata
+
+
+def csr_to_ig(csr: scipy.sparse.csr_matrix, directed=False, weighted=True):
+    rows, cols = csr.nonzero()
+
+    g = ig.Graph(directed=directed)
+    g.add_vertices(csr.shape[0])
+    g.add_edges(list(zip(rows, cols)))
+
+    if weighted:
+        weights = csr.data
+    else:
+        weights = np.ones_like(csr.data)
+
+    g.es["weight"] = weights
+    return g
+
+def get_leiden_membership(G, resolution: float = 1):
+    partition = la.find_partition(
+        G,
+        la.RBConfigurationVertexPartition,
+        weights=G.es["weight"],
+        resolution_parameter=resolution,
+    )
+    return partition.membership
+
+def get_igraph_membership(G, resolution: float = 1):
+    return G.community_leiden(objective_function="modularity", resolution=resolution, weights="weight").membership
+
+def get_kmeans_membership(data, n_clusters: int = 20):
+    from sklearn.cluster import KMeans
+
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(data)
+    return kmeans.labels_
---
+++ src/ai4bmr_learn/datamodules/NestedCV.py
@@ -0,0 +1,37 @@
+from ..data.splits import create_nested_cv_datasets
+
+
+class NestedCV:
+
+    def __init__(self, *,
+                 metadata: pd.DataFrame,
+                 num_outer_cv: int = 5,
+                 num_inner_cv: int = 5,
+                 target_column_name: str = "target",
+                 test_size: float | None = None,
+                 val_size: float | None = None,
+                 stratify: bool = False,
+                 random_state: int = 42,
+                 save_dir: Path):
+
+        self.num_outer_cv = num_outer_cv
+        self.num_inner_cv = num_inner_cv
+        self.save_dir = save_dir
+
+        create_nested_cv_datasets(metadata=metadata,
+                                  num_outer_cv=self.num_outer_cv,
+                                  num_inner_cv=self.num_inner_cv,
+                                  target_column_name=target_column_name,
+                                  test_size=test_size,
+                                  val_size=val_size,
+                                  stratify=stratify,
+                                  random_state=random_state,
+                                  save_dir=save_dir,
+                                  )
+
+    def get_dataset(self, outer_fold: int, inner_fold: int = None) -> pd.DataFrame:
+        path = self.save_dir / f"outer_fold={outer_fold}-inner_fold={inner_fold}.parquet" if inner_fold else self.save_dir / f"outer_fold={outer_fold}.parquet"
+        metadata = pd.read_parquet(path)
+        return metadata
+
+
