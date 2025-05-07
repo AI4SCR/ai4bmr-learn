bd2ba1afe17209d65f73c1d1f03cddf011f1b29a
Adriano Martinelli <adrianom@student.ethz.ch>
2025-05-07 18:53:29 +0200

    fix chunked

--- src/ai4bmr_learn/utils/utils.py
+++ src/ai4bmr_learn/utils/utils.py
@@ -3,18 +3,25 @@ from itertools import islice
 
 T = TypeVar('T')
 
+
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-def batched(iterable, n: int):
-    for i in range(0, len(iterable), n):
-        yield iterable[i : i + n]
 
-def chunked(iterable: Iterable[T], n: int) -> Iterator[List[T]]:
-    """Split iterable into successive non-overlapping chunks of size n."""
+def batched(iterable: Iterable[T], n: int) -> Iterator[List[T]]:
+    """Split iterable into successive non-overlapping batches of size n."""
     it = iter(iterable)
     while chunk := list(islice(it, n)):
         yield chunk
+
+
+def chunked(iterable: Iterable[T], n: int) -> List[List[T]]:
+    """Split an iterable into n chunks, trying to balance their lengths."""
+    items = list(iterable)
+    k, m = divmod(len(items), n)
+    n += m > 0
+    return [items[i * k:(i + 1) * k] for i in range(n)]
+
 
 def get_batch_size(iterable, n: int):
     return (len(iterable) + n - 1) // n
