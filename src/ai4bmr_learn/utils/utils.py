060d4eac22ca8228f7beb63c0da23e909f4e5d44
Adriano Martinelli <adrianom@student.ethz.ch>
2025-05-07 13:38:59 +0200

    add chunked and batched

--- src/ai4bmr_learn/utils/utils.py
+++ src/ai4bmr_learn/utils/utils.py
@@ -1,3 +1,20 @@
+from typing import Iterable, Iterator, TypeVar, List
+from itertools import islice
+
+T = TypeVar('T')
 
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
+
+def batched(iterable, n: int):
+    for i in range(0, len(iterable), n):
+        yield iterable[i : i + n]
+
+def chunked(iterable: Iterable[T], n: int) -> Iterator[List[T]]:
+    """Split iterable into successive non-overlapping chunks of size n."""
+    it = iter(iterable)
+    while chunk := list(islice(it, n)):
+        yield chunk
+
+def get_batch_size(iterable, n: int):
+    return (len(iterable) + n - 1) // n
