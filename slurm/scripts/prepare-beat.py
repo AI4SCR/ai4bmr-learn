1b823d45ee6f172dc58d4ea05e491bb30a61de6c
Adriano Martinelli <adrianom@student.ethz.ch>
2025-06-19 01:27:06 +0200

    dirty train_mae_cords

---
+++ slurm/scripts/prepare-beat.py
@@ -0,0 +1,17 @@
+from ai4bmr_datasets import BEAT
+dm = self = BEAT()
+
+# dm.prepare_clinical()
+# dm.prepare_wsi()
+try:
+    dm.segment(model_name='hest', target_mpp=4)
+    dm.segment(model_name='hest', target_mpp=4, source_mpp=0.25)
+    dm.segment(model_name='grandqc', target_mpp=4)
+    dm.segment(model_name='grandqc', target_mpp=4, source_mpp=0.25)
+except Exception as e:
+    print(e)
+
+# %%
+# import openslide
+# slide = openslide.OpenSlide('/work/PRTNR/CHUV/DIR/rgottar1/spatial/data/beat/02_processed/datasets/beat/1IS2.0G.0/wsi.tiff')
+# slide.level_dimensions
