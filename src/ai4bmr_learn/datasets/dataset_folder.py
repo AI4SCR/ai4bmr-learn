ee3c799d4b2f3991b377632903b59cc436934a02
Adriano Martinelli <adrianom@student.ethz.ch>
2025-07-10 16:31:15 +0200

    DatasetFolder

--- src/ai4bmr_learn/datasets/dataset_folder.py
+++ src/ai4bmr_learn/datasets/dataset_folder.py
@@ -9,27 +9,28 @@ import pandas as pd
 
 class DatasetFolder(Dataset):
 
-    def __init__(self, dataset_dir: Path, transform=None):
+    def __init__(self, dataset_dir: Path, image_version: str, transform=None):
         super().__init__()
 
         self.dataset_dir = dataset_dir.resolve()
         assert dataset_dir.exists() and dataset_dir.is_dir()
 
-        self.image_dir = self.dataset_dir / "images"
+        self.image_version = image_version
+        self.image_dir = self.dataset_dir / "images" / image_version
         assert self.image_dir.exists() and self.image_dir.is_dir()
         self.masks_dir = self.dataset_dir / "masks"
         assert self.masks_dir.exists() and self.masks_dir.is_dir()
 
-        self.metadata_path = self.dataset_dir / "metadata.parquet"
-        self.metadata = pd.read_parquet(self.metadata_path)
-        self.metadata = self.metadata[['dx_name']].fillna('NaN')
+        self.image_paths = {i.stem: i for i in self.image_dir.glob("*.tiff")}
+        self.mask_paths = {i.stem: i for i in self.masks_dir.glob("*.tiff")}
+        self.sample_ids = sorted(set(self.image_paths) & set(self.mask_paths))
 
-        self.image_paths = sorted(self.image_dir.glob("*.tiff"))
-        self.mask_paths = sorted(self.masks_dir.glob("*.tiff"))
-        self.sample_ids = [i.stem for i in self.image_paths]
+        self.metadata_path = self.dataset_dir / "metadata.parquet"
+        metadata = pd.read_parquet(self.metadata_path)
+        self.metadata = metadata
 
         self.to_image = v2.Compose([
-            v2.ToImage,
+            v2.ToImage(),
             v2.ToDtype(torch.float32, scale=False),
         ])
 
@@ -46,10 +47,11 @@ class DatasetFolder(Dataset):
 
         # avoid: RuntimeError: Trying to resize storage that is not resizable
         mask_path = self.mask_paths[sample_id]
-        mask = self.to_mask(mask_path)
+        mask = tifffile.imread(mask_path)
+        mask = self.to_mask(mask)
         # mask = torch.randn((300, 300)).long()
 
-        metadata = self.metadata.loc[sample_id].to_dict()
+        metadata = self.metadata.loc[sample_id].fillna('NaN').to_dict()
 
         # FIXME: including masks triggers: RuntimeError: Trying to resize storage that is not resizable
         # item = {'image': image, 'mask': mask, 'clinical': clinical}
