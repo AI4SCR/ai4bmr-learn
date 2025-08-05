e8ffffe41420ff59c0134e1180b0af77cf9c919c
Adriano Martinelli <adrianom@student.ethz.ch>
2025-08-05 21:37:20 +0200

    mil with collate

--- src/ai4bmr_learn/datasets/MIL.py
+++ src/ai4bmr_learn/datasets/MIL.py
@@ -1,7 +1,10 @@
+from typing import Callable
+
 import pandas as pd
 from torch.utils.data import Dataset
 from pathlib import Path
 import numpy as np
+from tqdm import tqdm
 
 class MIL(Dataset):
 
@@ -21,17 +24,23 @@ class MIL(Dataset):
     def __getitem__(self, idx):
         pass
 
+from torch.utils.data._utils.collate import collate, default_collate_fn_map
 
 class MILFromDataset(Dataset):
-    def __init__(self, dataset: Dataset,
+    def __init__(self, dataset: Dataset, collator: Callable | None | bool = None,
                  num_instances: int | None = None, shuffle: bool = False, random_state: int | None = None,
-                 bag_id_attr: str = 'bag_ids', bag_id_key: str | None = None):
+                 bag_id_attr: str = 'bag_ids', bag_id_key: str | None = None, cache_path: Path | None = None):
 
         self.dataset = dataset
+        if collator is None and not False:
+            self.collator = lambda x: collate(x, collate_fn_map=default_collate_fn_map)
+        else:
+            self.collator = collator
 
         self.bag_id_attr = bag_id_attr
         self.bag_id_key = bag_id_key  # optional key that might be present in the item to assert correctness
-        self.bag_ids: list[str|int] | None = None
+        self.bag_ids: list[str | int] | None = None
+        self.cache_dir = cache_path
 
         self.num_instances = num_instances
         self.shuffle = shuffle
@@ -47,9 +56,12 @@ class MILFromDataset(Dataset):
         else:
             raise ValueError(f"Dataset must have {self.bag_id_attr} attribute that identifies the bag ids.")
 
+        if self.cache_dir is not None:
+            self.cache_dir.mkdir(parents=True, exist_ok=True)
+
     def __getitem__(self, idx):
         bag_id = self.bag_ids[idx]
-        bag_idc = np.flatnonzero(self.bag_ids == bag_id)
+        bag_idc = np.flatnonzero(np.array(self.dataset.bag_ids) == bag_id)
 
         if self.shuffle:
             self.rng.shuffle(bag_idc)
@@ -58,11 +70,14 @@ class MILFromDataset(Dataset):
         bag_idc = bag_idc[:num_instances]
 
         instances = []
-        for i, idx in enumerate(bag_idc):
+        for idx in tqdm(bag_idc):
             item = self.dataset[idx]
             if self.bag_id_key is not None:
                 assert item[self.bag_id_key] == bag_id
             instances.append(item)
+
+        if self.collator is not False:
+            instances = self.collator(instances)
 
         return instances
 
