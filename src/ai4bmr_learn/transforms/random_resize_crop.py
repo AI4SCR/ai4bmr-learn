07fcee34cd524c97500efb81817e6d4acfccb7d3
Adriano Martinelli <adrianom@student.ethz.ch>
2025-08-17 13:47:15 +0200

    use actuall image size to compute transcripts tokens

--- src/ai4bmr_learn/transforms/random_resize_crop.py
+++ src/ai4bmr_learn/transforms/random_resize_crop.py
@@ -1,62 +1,84 @@
-from shapely.affinity import scale, translate
-from torch import nn
+
+import geopandas as gpd
+import numpy as np
 from torchvision.transforms import v2
 
 
-class RandomResizeCrop(nn.Module):
-    def __init__(self, **kwargs):
-        super().__init__()
+class RandomResizeCrop:  # this doesn't have to be a nn.Module
+    def __init__(self, errors: str = 'clip-raise', **kwargs):
         self.random_resize_crop = v2.RandomResizedCrop(**kwargs)
+        self.errors = errors
 
-    def forward(self, item):
+    def __call__(self, item: dict) -> dict:
         # NOTE: we cannot overwrite the `transform` call as suggested by the v2 documentation.
         #   the reason is that only torch.tensors, Images, Masks,... are passed to transform.
         #   and I could not find how one could configure the transform to accept other types without hacking the types.
 
         # FIXME: do we overwrite the input batch or do we need to create a copy?
-        result = {k:v for k,v in item.items()}
+        result = item.copy()
 
-        # NOTE: we need to create the params ourselves to be able to apply them to the `points` data
+        # TODO: make independent on image or points, i.e. works if only one is provided.
         image = item['image']
         _, height, width = image.shape
 
+        # NOTE: we need to create the params ourselves to be able to apply them to the `points` data
         params = self.random_resize_crop.make_params(image)
         result['image'] = self.random_resize_crop.transform(image, params=params)
 
-        # transform points
-        xmin = params['left']
-        xmax = params['left'] + params['width']
-        ymin = params['top']
-        ymax = params['top'] + params['height']
+        pts = item['points']
+        if not len(pts):
+            result['points'] = pts
+            result.setdefault('metadata', {}).setdefault('transform', {})['RandomResizeCrop'] = params
+            return result
 
-        points = item['points']
-        filter_ = points.geometry.x.between(xmin, xmax) & points.geometry.y.between(ymin, ymax)
-        points = points[filter_]
+        # pull coords once
+        xs = pts.geometry.x.to_numpy(copy=False)
+        ys = pts.geometry.y.to_numpy(copy=False)
 
-        if len(points):
-            # scale/translate points to match the crop after resizing
-            xfact = self.random_resize_crop.size[0] / params['width']
-            yfact = self.random_resize_crop.size[1] / params['height']
+        # bbox filter
+        xmin, ymin = params['left'], params['top']
+        xmax = xmin + params['width']
+        ymax = ymin + params['height']
+        mask = (xs >= xmin) & (xs <= xmax) & (ys >= ymin) & (ys <= ymax)
 
-            xoff = -params['left']
-            yoff = -params['top']
+        if mask.any():
+            # slice once; copy=True so we don’t modify parent df
+            pts_crop = pts.loc[mask].copy()
 
-            points = points.assign(geometry=points['geometry'].map(
-                lambda geom: scale(
-                    translate(geom, xoff=xoff, yoff=yoff),
-                    xfact=xfact,
-                    yfact=yfact,
-                    origin=(0, 0)
-                )
-            ))
+            # translate + scale
+            dst_h, dst_w = self.random_resize_crop.size
 
-            assert points.geometry.y.min() >= 0
-            assert points.geometry.x.min() >= 0
-            assert points.geometry.y.max() <= self.random_resize_crop.size[0]
-            assert points.geometry.x.max() <= self.random_resize_crop.size[1]
+            xoff, yoff = -xmin, -ymin
+            xfact = dst_w / params['width']
+            yfact = dst_h / params['height']
 
-        result['points'] = points
+            xs_m = (xs[mask] + xoff) * xfact
+            ys_m = (ys[mask] + yoff) * yfact
+
+            if 'clip' in self.errors:
+                xs_m = np.clip(xs_m, 0, dst_w)
+                ys_m = np.clip(ys_m, 0, dst_h)
+
+            if 'raise' in self.errors:
+                atol = 1e-6
+                xmin, ymin = xs_m.min(), ys_m.min()
+                xmax, ymax = xs_m.max(), ys_m.max()
+
+                if xmin < 0:
+                    assert np.isclose(xmin, 0, rtol=0, atol=atol), f'xmin={xmin}'
+                if ymin < 0:
+                    assert np.isclose(ymin, 0, rtol=0, atol=atol), f'ymin={ymin}'
+                if xmax > dst_w:
+                    assert np.isclose(xmax, dst_w, rtol=0, atol=atol), f'xmax={xmax} > {dst_w}'
+                if ymax > dst_h:
+                    assert np.isclose(ymax, dst_h, rtol=0, atol=atol), f'ymax={ymax} > {dst_h}'
+
+            # rebuild geometry in one go
+            pts_crop['geometry'] = gpd.points_from_xy(xs_m, ys_m)
+            result['points'] = pts_crop
+        else:
+            # no points survive
+            result['points'] = pts.iloc[[]]
+
         result.setdefault('metadata', {}).setdefault('transform', {})['RandomResizeCrop'] = params
-
         return result
-    
