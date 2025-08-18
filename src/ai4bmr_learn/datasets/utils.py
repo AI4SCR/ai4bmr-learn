3765b90490b7d5b11da2b7e3cfbb2ad9a9fac1c9
adrianomartinelli <adrianom@student.ethz.ch>
2025-08-18 21:56:38 +0200

    fix: detach and move to cpu before save; prepare for metadata; factour our metadata filtering

--- src/ai4bmr_learn/datasets/utils.py
+++ src/ai4bmr_learn/datasets/utils.py
@@ -1,94 +1,51 @@
-from pathlib import Path
-
+import pandas as pd
 from loguru import logger
 
-from ai4bmr_learn.data.splits import Split, generate_splits
+from ai4bmr_learn.data.splits import Split
 
 
-def create_nested_cv_datasets(
-        dataset,
+def filter_items_and_metadata(
+        items_ids: list[str | int],
+        metadata: pd.DataFrame,
         *,
-        test_size: float = 0.2,
-        val_size: float = 0.2,
-        num_outer_cv: int = 5,
-        num_inner_cv: int = 5,
-        stratify: bool = False,
-        save_dir: Path,
+        split: str | None = None,
+        drop_nan_columns: bool = False,
 ):
-    save_dir.mkdir(parents=True, exist_ok=True)
-    logger.info(f"Creating folds in {save_dir}")
+    """
+    Filters a list of item dicts and metadata based on a split value and optionally drops
+    NaN-containing columns in the metadata. Returns (items, item_ids, metadata).
+    """
 
-    metadata = dataset.metadata
-    target_column_name = dataset.target_column_name
-    for outer_fold in range(num_outer_cv):
-        outer_metadata = generate_splits(
-            metadata,
-            target_column_name=target_column_name,
-            test_size=test_size,
-            stratify=stratify,
-            random_state=outer_fold,
+    # filter item ids
+    if split is not None:
+        keep = metadata[Split.COLUMN_NAME.value] == split
+        assert keep.sum() > 0, f"There are no items that belong to split='{split}'"
+
+        metadata = metadata[keep]
+        valid_ids = set(metadata.index)
+    else:
+        valid_ids = set(metadata.index)
+
+    valid_item_ids = [i for i in items_ids if i in valid_ids]
+
+    assert len(metadata) == len(valid_item_ids), "Metadata and items differ"
+    logger.info(f"Filtered items and metadata for split='{split}'. Found {len(valid_item_ids)} items.")
+
+    # drop NaN
+    filter_ = metadata.isna().any()
+    cols_with_nan = metadata.columns[filter_].tolist()
+    if filter_.any():
+        if drop_nan_columns:
+            metadata = metadata.drop(columns=cols_with_nan)
+
+        msg = (
+                f"Detected NaN values in the metadata which is incompatible with torch.DataLoader. "
+                + ("Dropping them." if drop_nan_columns else "Use drop_nan_columns=True to drop them.")
+                + f"Affected columns: {cols_with_nan}. "
         )
-        outer_metadata.to_parquet(save_dir / f"outer_fold={outer_fold}.parquet", engine="fastparquet")
+        logger.warning(msg)
 
-        for inner_fold in range(num_inner_cv):
-            filter_ = outer_metadata[Split.COLUMN_NAME] == Split.TRAIN
-            inner_metadata = outer_metadata[filter_]
-            inner_metadata = generate_splits(
-                inner_metadata,
-                target_column_name=target_column_name,
-                test_size=val_size,
-                val_size=val_size,
-                stratify=stratify,
-                random_state=inner_fold,
-            )
+        if split is not None and Split.COLUMN_NAME.value in cols_with_nan:
+            logger.warning("Detected NaN in column 'split'. This is most likely a bug.")
 
-            inner_metadata.to_parquet(
-                save_dir / f"outer_fold={outer_fold}-inner_fold={inner_fold}.parquet", engine="fastparquet"
-            )
-
-
-from torch.utils.data import DataLoader
-from torch import get_num_threads
-
-
-class DataLoaderMixin:
-
-    def __init__(self,
-                 batch_size: int = 64,
-                 num_workers: int = None,
-                 persistent_workers: bool = True,
-                 shuffle: bool = True,
-                 pin_memory: bool = True):
-        self.batch_size = batch_size
-        self.num_workers = num_workers if num_workers is not None else max(0, get_num_threads() - 1)
-        self.persistent_workers = persistent_workers if self.num_workers > 0 else False
-        self.shuffle = shuffle
-        self.pin_memory = pin_memory
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_set,
-            batch_size=self.batch_size,
-            shuffle=self.shuffle,
-            num_workers=self.num_workers,
-            persistent_workers=self.persistent_workers,
-            pin_memory=self.pin_memory,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            persistent_workers=self.persistent_workers,
-            pin_memory=self.pin_memory,
-        )
-
-    def test_dataloader(self):
-        return DataLoader(
-            self.test_set,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            persistent_workers=self.persistent_workers,
-            pin_memory=self.pin_memory,
-        )
+    return valid_item_ids, metadata
