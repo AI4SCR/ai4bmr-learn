56d1ad37ed35f2ffa83a22794a8288b4dad9d23a
Adriano Martinelli <adrianom@student.ethz.ch>
2025-11-07 00:10:49 +0100

    add confusion matrix, introduce weighted Crossentropy, and use multiclass metrics

--- src/ai4bmr_learn/metrics/classification.py
+++ src/ai4bmr_learn/metrics/classification.py
@@ -2,27 +2,19 @@ from torchmetrics import Accuracy, F1Score, MetricCollection, Precision, Recall
 
 
 def get_metric_collection(num_classes: int) -> MetricCollection:
-    if num_classes > 2:
-        collection = MetricCollection(
-            {
-                "accuracy-micro": Accuracy(task="multiclass", average="micro", num_classes=num_classes),
-                "accuracy-macro": Accuracy(task="multiclass", average="macro", num_classes=num_classes),
-                "recall-micro": Recall(task="multiclass", average="micro", num_classes=num_classes),
-                "recall-macro": Recall(task="multiclass", average="macro", num_classes=num_classes),
-                "precision-micro": Precision(task="multiclass", average="micro", num_classes=num_classes),
-                "precision-macro": Precision(task="multiclass", average="macro", num_classes=num_classes),
-                "f1-weighted": F1Score(task="multiclass", average="weighted", num_classes=num_classes),
-                "f1-micro": F1Score(task="multiclass", average="micro", num_classes=num_classes),
-                "f1-macro": F1Score(task="multiclass", average="macro", num_classes=num_classes),
-            },
-        )
-    else:
-        collection = MetricCollection({
-            "accuracy": Accuracy(task='binary'),
-            "recall": Recall(task='binary'),
-            "precision": Precision(task='binary'),
-            "f1": F1Score(task='binary'),
-        })
+    collection = MetricCollection(
+        {
+            "accuracy-micro": Accuracy(task="multiclass", average="micro", num_classes=num_classes),
+            "accuracy-macro": Accuracy(task="multiclass", average="macro", num_classes=num_classes),
+            "recall-micro": Recall(task="multiclass", average="micro", num_classes=num_classes),
+            "recall-macro": Recall(task="multiclass", average="macro", num_classes=num_classes),
+            "precision-micro": Precision(task="multiclass", average="micro", num_classes=num_classes),
+            "precision-macro": Precision(task="multiclass", average="macro", num_classes=num_classes),
+            "f1-weighted": F1Score(task="multiclass", average="weighted", num_classes=num_classes),
+            "f1-micro": F1Score(task="multiclass", average="micro", num_classes=num_classes),
+            "f1-macro": F1Score(task="multiclass", average="macro", num_classes=num_classes),
+        },
+    )
     return collection
 
 
