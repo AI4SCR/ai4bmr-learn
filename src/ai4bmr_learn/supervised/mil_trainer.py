321ae67ab8b0e435f4e6405771c31fba4797ed31
Adriano Martinelli <adrianom@student.ethz.ch>
2025-12-23 09:30:01 +0100

    simplify MILTrainerExtended

--- src/ai4bmr_learn/supervised/mil_trainer.py
+++ src/ai4bmr_learn/supervised/mil_trainer.py
@@ -97,46 +97,12 @@ class MILTrainerExtended(L.LightningModule):
 
         self.save_hyperparameters(ignore=["backbone", "mil", "head"])
 
-    def shared_step_for_list(self, batch: list, batch_idx: int | None = None):
-        raise(NotImplementedError("This method is not harmonized. Use shared_step_for_dict instead."))
-
-        zs = []
-        targets = []
-        # NOTE: we need to feed the bags separately to the backbone, since they are designed for [B, D] inputs
-        for i, bag in enumerate(batch):
-            target = glom(bag, self.target_key)
-            target = torch.unique(target).item()
-            targets.append(target)
-
-            data = glom(bag, self.batch_key) if self.batch_key else bag
-
-            z = self.backbone(**data) if self.as_kwargs else self.backbone(data)
-            z = pool(z, strategy=self.pooling)
-            zs.append(z)
-
-        zs = torch.stack(zs)  # B, M, R
-        targets = torch.tensor(targets, dtype=torch.long, device=self.device)
-        assert zs.shape[0] == targets.shape[0]
-
-        # z, attn, attn_logits = self.mil(data)
-        logits = self.head(zs)
-        loss = self.criterion(logits, targets)
-
-        return zs, logits, targets, loss, attn, attn_logits
-
-    def shared_step_for_dict(self, batch, batch_idx: int | None = None):
+    def shared_step(self, batch, batch_idx: int | None = None):
         bags = glom(batch, self.batch_key)
-        attentions = glom(batch, self.attention_key, default=None)
         assert isinstance(bags, torch.Tensor)
-        assert attentions is None or isinstance(attentions, torch.Tensor)
-        if attentions is not None and not isinstance(bags, torch.Tensor):
-            raise NotImplementedError(
-                'Bags must be pure tensors if you want to use padding. Use `collate_fn=list` and pad=False instead.')
 
         targets = glom(batch, self.target_key)
         assert targets.ndim == 1  # [B,]
-        # targets = torch.unique(targets, dim=1)  # NOTE: we expect [B, M], # TODO: discuss this choice
-        # targets = targets.reshape(-1)
         assert len(targets) == len(bags)
 
         B, M, *D = bags.shape
@@ -144,10 +110,6 @@ class MILTrainerExtended(L.LightningModule):
         zs = []
         # NOTE: we need to feed the bags separately to the backbone, since they are designed for [B, D] inputs
         for i, bag in enumerate(bags):
-
-            if attentions is not None:
-                attention = attentions[i]
-                bag = bag[attention]
 
             z = self.backbone(**bag) if self.as_kwargs else self.backbone(bag)
             z = pool(z, strategy=self.pooling)
@@ -163,12 +125,6 @@ class MILTrainerExtended(L.LightningModule):
         loss = self.criterion(logits, targets)
 
         return z, logits, targets, loss, attn, attn_logits
-
-    def shared_step(self, batch, batch_idx: int | None = None):
-        if isinstance(batch, list):
-            return self.shared_step_for_list(batch, batch_idx=batch_idx)
-        elif isinstance(batch, dict):
-            return self.shared_step_for_dict(batch, batch_idx=batch_idx)
 
     def on_train_epoch_start(self):
         self.train_stats['train/num_samples'] = 0
@@ -190,13 +146,15 @@ class MILTrainerExtended(L.LightningModule):
         self.train_stats['class_cnt'] = Counter()
 
     def training_step(self, batch, batch_idx):
+        # if batch_idx == 0:
+        #     torch.save(batch, '/work/FAC/FBM/DBC/mrapsoma/prometex/projects/mesothelioma/workflow-xe-hne/2-mil-end-to-end/train-batch.pt')
         z, logits, targets, loss, attn, attn_logits = self.shared_step(batch)
         batch_size = targets.shape[0]
 
         # metrics
         preds = torch.argmax(logits, dim=1).long()
         self.train_metrics(logits, targets)
-        self.log_dict(self.train_metrics, on_step=True, on_epoch=True, batch_size=batch_size)
+        self.log_dict(self.train_metrics, on_step=True, on_epoch=False, batch_size=batch_size)
 
         self.cm_train(preds, targets)
 
@@ -207,12 +165,9 @@ class MILTrainerExtended(L.LightningModule):
         self.train_stats['train/num_samples'] += batch_size
         self.train_stats['class_cnt'].update(targets.tolist())
 
-        if isinstance(batch, list):
-            return loss
-        else:
-            batch['loss'] = loss
-            batch['z'] = z.detach().cpu()
-            return batch
+        batch['loss'] = loss
+        batch['z'] = z.detach().cpu()
+        return batch
 
     def on_validation_epoch_start(self):
         self.val_stats['val/num_samples'] = 0
@@ -231,24 +186,12 @@ class MILTrainerExtended(L.LightningModule):
         self.logger.experiment.log({'val/confusion_matrix': fig})
         plt.close('all')
 
-        # added code to save attention weights
-        # Save accumulated attention weights to disk
-
-        if self.validation_step_outputs and not self.trainer.fast_dev_run:
-            import os
-            # Try to get the logger's save directory (works for WandB)
-            save_dir = getattr(self.logger, "save_dir", ".")
-            if hasattr(self.logger, "experiment") and hasattr(self.logger.experiment, "dir"):
-                save_dir = self.logger.experiment.dir
-
-            save_path = os.path.join(save_dir, f"val_attn_epoch_{self.current_epoch}.pt")
-            torch.save(self.validation_step_outputs, save_path)
-            self.validation_step_outputs = {}  # Clear memory
-
         self.val_stats['val/num_samples'] = 0
         self.val_stats['class_cnt'] = Counter()
 
     def validation_step(self, batch, batch_idx):
+        # if batch_idx == 0:
+        #     torch.save(batch, '/work/FAC/FBM/DBC/mrapsoma/prometex/projects/mesothelioma/workflow-xe-hne/2-mil-end-to-end/val-batch.pt')
         z, logits, targets, loss, attn, attn_logits = self.shared_step(batch)
         batch_size = targets.shape[0]
 
@@ -260,64 +203,14 @@ class MILTrainerExtended(L.LightningModule):
         self.cm_val(preds, targets)
 
         # loss
-        self.log("loss/val", loss, batch_size=batch_size)
+        self.log("loss/val", loss, on_epoch=True, batch_size=batch_size)
 
         # stats
         self.val_stats['val/num_samples'] += batch_size
         self.val_stats['class_cnt'].update(targets.tolist())
 
-        if isinstance(batch, list):
-            return loss
-        else:
-            batch['loss'] = loss
-            batch['z'] = z.detach().cpu()
-            batch['attn'] = attn.detach().cpu()  # added attn
-            batch['attn_logits'] = attn_logits.detach().cpu()
-            return batch
-
-    def test_step(self, batch, batch_idx):
-        z, logits, targets, loss, attn, attn_logits = self.shared_step(batch, batch_idx)
-        batch_size = targets.shape[0]
-
-        # metrics
-        preds = torch.argmax(logits, dim=1).long()
-        self.test_metrics(logits, targets)
-        self.log_dict(self.test_metrics, batch_size=batch_size)
-
-        self.cm_test(preds, targets)
-
-        # loss
-        self.log("loss/test", loss, batch_size=batch_size)
-
         batch['loss'] = loss
-        batch['repr'] = z.detach().cpu()
-        batch['attn'] = attn.detach().cpu()  # added attn
-        batch['attn_logits'] = attn_logits.detach().cpu()
-        return batch
-
-    def on_test_end(self) -> None:
-        fig, ax = self.cm_test.plot()
-        self.cm_test.reset()
-        self.logger.experiment.log({'test/confusion_matrix': fig})
-        plt.close('all')
-
-    def predict_step(self, batch, batch_idx):
-        data = glom(batch, self.batch_key)
-
-        zs = []
-        for bag in data:
-            z = self.backbone(bag)
-            z = pool(z, strategy=self.pooling)
-            z.append(z)
-        zs = torch.stack(zs)
-        assert zs.shape[0] == B
-
-        logits = self.head(zs)
-        preds = logits.argmax(dim=1).long()
-
-        batch["prediction"] = preds.detach().cpu()
-        batch["logits"] = logits.detach().cpu()
-        batch['embedding'] = z.detach().cpu()
+        batch['z'] = z.detach().cpu()
         return batch
 
     def configure_optimizers(self):
