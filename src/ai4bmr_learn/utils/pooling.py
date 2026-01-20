5def35b7ae57820d9908287b79b7339026722eb0
Adriano Martinelli <adrianom@student.ethz.ch>
2026-01-20 16:22:57 +0100

    update pooling

--- src/ai4bmr_learn/utils/pooling.py
+++ src/ai4bmr_learn/utils/pooling.py
@@ -1,11 +1,18 @@
 def pool(x, strategy: str):
     if strategy is None:
         return x
-    elif strategy == 'cls':
-        return x[:, 0]
-    elif strategy == 'flatten':
-        return x.flatten(start_dim=1)
-    elif strategy == 'avg':
-        return x.mean(dim=1)
-    else:
-        raise NotImplementedError(f'{strategy} is not implemented.')
+
+    assert x.ndim == 3, f'Expected input with 3 dimensions, got {x.ndim} dimensions.'
+    match strategy:
+        case 'cls':
+            return x[:, 0]
+        case 'token':
+            return x[:, 0]
+        case 'flatten':
+            return x.flatten(start_dim=1)
+        case 'avg':
+            return x.mean(dim=1)
+        case "max":
+            return x.amax(dim=1)
+        case _:
+            raise NotImplementedError(f'{strategy} is not implemented.')
