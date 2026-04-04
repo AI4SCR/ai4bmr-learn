e9b5d87f4e9d16885ab2c0a939f6ebfefb15303b
adrianomartinelli <adrianom@student.ethz.ch>
2026-04-04 10:04:31 +0200

    Restore pair utility compatibility import

--- src/ai4bmr_learn/utils/__init__.py
+++ src/ai4bmr_learn/utils/__init__.py
@@ -1,3 +1,4 @@
 from ai4bmr_learn.utils.images import get_thumbnail, get_thumbnail_size_and_scale
+from ai4bmr_learn.utils.utils import pair
 
-__all__ = ["get_thumbnail", "get_thumbnail_size_and_scale"]
+__all__ = ["get_thumbnail", "get_thumbnail_size_and_scale", "pair"]
